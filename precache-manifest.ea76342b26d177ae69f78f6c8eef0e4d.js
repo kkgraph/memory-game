0678ce5e024156f00308a2adbb1ef084d67e407e
Kim Graff <kimagraff@gmail.com>
2019-09-20 19:37:50 -0400

    Updates

---
+++ precache-manifest.ea76342b26d177ae69f78f6c8eef0e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f397dca126f7eabc7318a5e72d732ad0",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "b462fdca0991c0f92173",
+    "url": "/memory-game/static/css/main.96dc8260.chunk.css"
+  },
+  {
+    "revision": "f6366c32741f75246564",
+    "url": "/memory-game/static/js/2.3814e01f.chunk.js"
+  },
+  {
+    "revision": "b462fdca0991c0f92173",
+    "url": "/memory-game/static/js/main.4be6177c.chunk.js"
+  },
+  {
+    "revision": "a965dad33cfc69e1449b",
+    "url": "/memory-game/static/js/runtime~main.f417e672.js"
+  }
+]);
